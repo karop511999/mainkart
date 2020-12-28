e3b27046dfc09b96a334e35bbcd86327f6a0d005
karop511999 <karopdezosa.sebastian@nutechnologyinc.com>
2020-12-28 14:47:17 +0530

    order

--- src/App.js
+++ src/App.js
@@ -13,8 +13,23 @@ class Flipkart
     return "my name is:"+this.name;
   }
 }
+class Fkart extends Flipkart
+{
+  constructor(item,name)
+  {
+    super(name);
+    this.item=item;
+  }
+  banner()
+  {
+    let print=super.getFlipkart();
+    print+=" your order item is "+this.item;
+    return print;
+  }
+}
 function App() {
-  const fname=new Flipkart("karop");
+  const f1=new Fkart("headphone","karop");
+  const f2=new Fkart("tv","karop")
   const [Toggle,A]=useState(true);
   const click=()=>{
    const presentState=Toggle;
@@ -24,15 +39,19 @@ function App() {
   if(Toggle){
     displayContent="closed";
   }
+ 
   return (
-   <div>
-     <button onClick={click}>click</button>
-     <div className="content">
-       {displayContent}
-      
-     </div>
-     {fname.getFlipkart()}
-   </div>
+    <div>
+      <button onClick={click}>Toggle</button>
+      <div className="content">
+        {displayContent}
+      </div>
+      {f1.banner()}
+      <div>
+      {f2.banner()}
+      </div>
+    
+    </div>
   );
 }
 
